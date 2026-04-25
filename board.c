a05c1b7bc741cdcc6e26cf198e842d9d848a7d11
Elio Fiorentini <efiorent@student.42lyon.fr>
2026-04-25 12:32:11 +0200

    Initial commit

---
+++ board.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   board.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efiorent <efiorent@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/25 12:11:26 by efiorent          #+#    #+#             */
+/*   Updated: 2026/04/25 12:31:46 by efiorent         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "board.h"
+#include <assert.h>
+
+void	init_board(t_board *board, int size)
+{
+	assert((size < 4 || size > 5) && "Invalid board size");
+	board->size = size;
+	for (size_t i = 0; i < size; i++)
+		for (size_t j = 0; j < size; j++)
+			board->board[i][j] = 0;
+}
---
+++ board.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   board.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efiorent <efiorent@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/25 11:54:05 by efiorent          #+#    #+#             */
+/*   Updated: 2026/04/25 12:22:48 by efiorent         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BOARD_H
+# define BOARD_H
+# include <stddef.h>
+
+typedef struct s_board
+{
+	int		board[5][5];
+	size_t	size;
+}			t_board;
+
+void	init_board(t_board *board, int size);
+
+#endif
